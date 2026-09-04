b5355d8e86b9d0b85d033df67acf09865cf22182
Ryuk0x01 <hachimisoufain11@gmail.com>
2026-09-04 20:00:08 +0100

    Remove base path for root domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ryuk0x01.github.io/",
-	base: "/fuwari/",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
